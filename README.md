2e53ae5142c84ec5a2d71d7369b2b26816dabdad
Eric Peterson <eric@elpete.com>
2016-11-03 14:08:30 -0600

    Convert to ColdBox Elixir

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
-# laravel-elixir-vue-2
+# extension-vue-2
 
-> Laravel Elixir 6 support for Vue 2.0
+> ColdBox Elixir 6 support for Vue 2.0
 
-This package provides Webpack support for compiling Vue 2.0 `.vue` files in your Laravel Elixir 6 projects. Please note that, if using Laravel Elixir 5 or below, you don't want this package.
+This package provides Webpack support for compiling Vue 2.0 `.vue` files in your ColdBox Elixir 2 projects. Please note that, if using ColdBox Elixir 1, you don't want this package.
 
-Usage is identical to [laravel-elixir-vue](https://github.com/JeffreyWay/laravel-elixir-vue):
+Usage is identical to [coldbox-elixir-vue](https://github.com/coldbox-elixir/extension-vue):
 
 ## Step 1: Install
 
 ```
-npm install laravel-elixir-vue-2 --save-dev
+npm install coldbox-elixir-vue-2 --save-dev
 ```
 
 ## Step 2: Gulpfile.js
 
 ``` js
-var elixir = require('laravel-elixir')
+var elixir = require( "coldbox-elixir" );
 
-require('laravel-elixir-vue-2')
+require( "coldbox-elixir-vue-2" );
 
-elixir(function(mix) {
-  mix.webpack('app.js'); // resources/assets/js/app.js
-})
+elixir( function( mix ) {
+	mix.webpack( "app.js" );
+} )
 ```
--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-var Elixir = require('laravel-elixir')
+var Elixir = require( "coldbox-elixir" );
 
-Elixir.ready(function () {
-  Elixir.webpack.mergeConfig({
-    // ensure we are using the version of Vue that supports templates
-    resolve: {
-      alias: {
-        vue: 'vue/dist/vue.js'
-      },
-      extensions: ['.js', '.vue']
-    },
-    // use buble loader since it is the default in Elixir
-    vue: {
-      loaders: {
-        js: 'buble-loader'
-      }
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.vue$/,
-          loader: 'vue-loader'
+Elixir.ready( function() {
+    Elixir.webpack.mergeConfig( {
+        // ensure we are using the version of Vue that supports templates
+        resolve: {
+            alias: {
+                vue: "vue/dist/vue.js"
+            },
+            extensions: [ ".js", ".vue" ]
         },
-        {
-          test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-          loader: 'file-loader',
-          query: {
-            limit: 10000,
-            name: '../img/[name].[hash:7].[ext]'
-          }
+        // use buble loader since it is the default in Elixir
+        vue: {
+            loaders: {
+                js: "buble-loader"
+            }
         },
-        {
-          test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-          loader: 'url-loader',
-          query: {
-            limit: 10000,
-            name: '../fonts/[name].[hash:7].[ext]'
-          }
+        module: {
+            loaders: [
+                {
+                    test: /\.vue$/,
+                    loader: "vue-loader"
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                    loader: "file-loader",
+                    query: {
+                        limit: 10000,
+                        name: "../img/[name].[hash:7].[ext]"
+                    }
+                },
+                {
+                    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                    loader: "url-loader",
+                    query: {
+                        limit: 10000,
+                        name: "../fonts/[name].[hash:7].[ext]"
+                    }
+                }
+            ]
         }
-      ]
-    }
-  })
+    })
 })
